9ec7e866ad6ef2c89fb2e69703a90ca7be6db0ec
sasaki1999 <syole188@gmail.com>
2024-04-02 11:47:49 +0700

    update 1

--- src/test/java/com/poly/BookDatnApplicationTests.java
+++ src/test/java/com/poly/BookDatnApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FivePlusBookDatnApplicationTests {
+class BookDatnApplicationTests {
 
 	@Test
 	void contextLoads() {
