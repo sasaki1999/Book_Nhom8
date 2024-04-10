2dc8a595756f58f40c36bff19bc1730678543cbf
TranPhuocToan <tranphuoctoan12122003@gmail.com>
2024-04-10 21:30:35 +0700

    chỉnh thêm ảnh

--- src/main/java/com/poly/store/service/impl/UploadServiceImpl.java
+++ src/main/java/com/poly/store/service/impl/UploadServiceImpl.java
@@ -1,36 +1,53 @@
 package com.poly.store.service.impl;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.poly.service.UploadService;
 
-
 @Service
-public class UploadServiceImpl implements UploadService{
+public class UploadServiceImpl implements UploadService {
+	@Autowired
+	HttpServletRequest request;
 	@Autowired
 	ServletContext app;
 
 	@Override
 	public File save(MultipartFile file, String folder) {
-		File dir = new File(("D:\\Git\\DATN-BOOK\\Book_DATN\\src\\main\\webapp\\images\\" + folder));
-		if (!dir.exists()) {
-			dir.mkdirs();
-		}
-		String s = System.currentTimeMillis() + file.getOriginalFilename();
-		String name = Integer.toHexString(s.hashCode()) + s.substring(s.lastIndexOf("."));
+
 		try {
-			File savedFile = new File(dir, name);
-			file.transferTo(savedFile);
-			System.out.println(savedFile.getAbsolutePath());
-			return savedFile;
-		} catch (Exception e) {
-			throw new RuntimeException(e);
+			Resource resource = new ClassPathResource("/static/images/");
+			String path = resource.getFile().getAbsolutePath() +"/";
+			System.out.println(path);
+			File dir = new File(path + folder);
+			if (!dir.exists()) {
+				dir.mkdirs();
+			}
+			String s = System.currentTimeMillis() + file.getOriginalFilename();
+			String name = Integer.toHexString(s.hashCode()) + s.substring(s.lastIndexOf("."));
+			try {
+				File savedFile = new File(dir, name);
+				file.transferTo(savedFile);
+				System.out.println(dir + "dir" + name + "name" + savedFile.getAbsolutePath());
+				return savedFile;
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		;
+		return null;
+
 	}
 }
